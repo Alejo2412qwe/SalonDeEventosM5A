c7ccf7bdd7c839e06d993d02dea185bed2c2a56e
Alejo2412qwe <111301530+Alejo2412qwe@users.noreply.github.com>
2023-06-26 08:24:36 -0500

    clases xd

--- SalonDeEventos/src/app/modelo/usuario.ts
+++ SalonDeEventos/src/app/modelo/usuario.ts
@@ -1,2 +1,31 @@
+import { Persona } from "./persona";
+import { Rol } from "./rol";
+
 export class Usuario {
+
+    usuId: number;
+    usuNombreUsuario;
+    usuContraseña: string;
+    usuFechaRegistro: Date;
+    usuPerId: Persona;
+    rolId: Rol;
+
+
+    constructor(
+        usuId: number,
+        usuNombreUsuario: string,
+        usuContraseña: string,
+        usuFechaRegistro: Date,
+        usuPerId: Persona,
+        rolId: Rol
+    ) {
+        this.usuId = usuId;
+        this.usuNombreUsuario = usuNombreUsuario;
+        this.usuContraseña = usuContraseña;
+        this.usuFechaRegistro = usuFechaRegistro;
+        this.usuPerId = usuPerId;
+        this.rolId = rolId;
+    }
+
+
 }
