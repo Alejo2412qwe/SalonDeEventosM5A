e669bbc2f5992ce9cf23e89ba2bc09f96ec42469
Alejo2412qwe <111301530+Alejo2412qwe@users.noreply.github.com>
2023-06-26 08:33:09 -0500

    dxxd

--- SalonDeEventos/src/app/modelo/cotizacion.ts
+++ SalonDeEventos/src/app/modelo/cotizacion.ts
@@ -2,29 +2,41 @@ import { Salon } from "./salon";
 import { Usuario } from "./usuario";
 
 export class Cotizacion {
-    cotiId: number = 0;
-    cotiTipoEvento: string = "";
-    cotiFechaEvento: Date = new Date(0);
-    cotiDescripcion: string = "";
-    cotiEstado: boolean = false;
-    cotiMonto: number = 0;
-    cotiHoraFin: Date = new Date(0);
-    cotiHoraInicio: Date = new Date(0);
-    cotiFechaRegistro: Date = new Date(0);
-    salId: Salon = new Salon;
-    usuId: Usuario = new Usuario;
+    cotiId: number;
+    cotiTipoEvento: string;
+    cotiFechaEvento: Date;
+    cotiDescripcion: string;
+    cotiEstado: boolean;
+    cotiMonto: number;
+    cotiHoraFin: Date;
+    cotiHoraInicio: Date;
+    cotiFechaRegistro: Date;
+    salId: Salon;
+    usuId: Usuario;
 
-    constructor() {
-        this.cotiId = 0;
-        this.cotiTipoEvento = "";
-        this.cotiFechaEvento = new Date(0);
-        this.cotiDescripcion = "";
-        this.cotiEstado = false;
-        this.cotiMonto = 0;
-        this.cotiHoraFin = new Date(0);
-        this.cotiHoraInicio = new Date(0);
-        this.cotiFechaRegistro = new Date(0);
-        this.salId = new Salon();
-        this.usuId = new Usuario();
+    constructor(
+        cotiId: number,
+        cotiTipoEvento: string,
+        cotiFechaEvento: Date,
+        cotiDescripcion: string,
+        cotiEstado: boolean,
+        cotiMonto: number,
+        cotiHoraFin: Date,
+        cotiHoraInicio: Date,
+        cotiFechaRegistro: Date,
+        salId: Salon,
+        usuId: Usuario
+    ) {
+        this.cotiId = cotiId;
+        this.cotiTipoEvento = cotiTipoEvento;
+        this.cotiFechaEvento = cotiFechaEvento;
+        this.cotiDescripcion = cotiDescripcion;
+        this.cotiEstado = cotiEstado;
+        this.cotiMonto = cotiMonto;
+        this.cotiHoraFin = cotiHoraFin;
+        this.cotiHoraInicio = cotiHoraInicio;
+        this.cotiFechaRegistro = cotiFechaRegistro;
+        this.salId = salId;
+        this.usuId = usuId;
     }
 }
