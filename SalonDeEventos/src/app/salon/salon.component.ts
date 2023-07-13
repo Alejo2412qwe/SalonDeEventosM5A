05808eb688cbcccd7a2a966cd04735107227cd2b
Alejo2412qwe <111301530+Alejo2412qwe@users.noreply.github.com>
2023-07-12 21:11:01 -0500

    salon service

--- SalonDeEventos/src/app/salon/salon.component.ts
+++ SalonDeEventos/src/app/salon/salon.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { SalonService } from '../service/salon.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { ImgSalonService } from '../service/imgSalon.service';
 import { Salon } from '../modelo/salon';
@@ -16,18 +16,23 @@ import { EmpresaService } from '../service/empresa.service';
 export class SalonComponent {
 
   constructor(private salonService: SalonService, private EmpresaService: EmpresaService,
-    private router: Router, private toastr: ToastrService, private imgSalonService: ImgSalonService) { }
+    private router: Router, private toastr: ToastrService, private imgSalonService: ImgSalonService, private activatedRoute: ActivatedRoute) { }
 
+  accion: string = "";
   seleccionados: Empresa = new Empresa;
   salon: Salon = new Salon();
   empresa: Empresa = new Empresa();
+
+  ngOnInit(): void {
+    this.cargarAccion()
+  }
 
   registrarSalon(): void {
 
     this.EmpresaService.getEmpresaPorId(1).subscribe(emp => {
 
       this.salon.empId = emp;
-      this.salon.salEstado=1;
+      this.salon.salEstado = 1;
       this.salonService.crearSalon(this.salon).subscribe(
         response => {
           Swal.fire({
@@ -39,6 +44,28 @@ export class SalonComponent {
             location.reload();
           })
         })
+    })
+  }
+
+  cargarSalon(): void {
+    this.activatedRoute.params.subscribe(params => {
+      let id = params['id']
+      if (id) {
+        this.salonService.getSalonId(id).subscribe((salon) => {
+          this.salon = salon
+        })
+      }
+    })
+  }
+
+
+  cargarAccion(): void {
+    this.activatedRoute.params.subscribe(params => {
+      this.accion = params['accion']
+      console.log(this.accion)
+      if (this.accion === 'editar') {
+        this.cargarSalon();
+      }
     })
   }
 }
