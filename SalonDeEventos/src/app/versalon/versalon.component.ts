779aaf48620c7e1e4d5e6b703769aa859b556f3f
D4N11EL <101025579+D4N11EL@users.noreply.github.com>
2023-07-20 18:48:29 -0500

    CAMBIOS

---
+++ SalonDeEventos/src/app/versalon/versalon.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-versalon',
+  templateUrl: './versalon.component.html',
+  styleUrls: ['./versalon.component.css']
+})
+export class VersalonComponent {
+
+}
--- SalonDeEventos/src/assets/js/default/ventana.js
+++ SalonDeEventos/src/assets/js/default/ventana.js
@@ -21,3 +21,39 @@ function mostrarVentanaFlotante() {
     const ventanaFlotante = document.querySelector('.ventana-flotante2');
     ventanaFlotante.style.display = 'none';
   }
+
+
+  function hola() {
+    var swiper = new Swiper(".swiper", {
+      effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 3,
+        slideShadows: true
+      },
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 2
+        },
+        768: {
+          slidesPerView: 1
+        },
+        1024: {
+          slidesPerView: 2
+        },
+        1560: {
+          slidesPerView: 3
+        }
+      }
+    });
+    
+  }  
